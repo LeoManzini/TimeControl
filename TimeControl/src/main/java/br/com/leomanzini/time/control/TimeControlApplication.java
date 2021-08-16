595cc845f661fc306e2d529d412f0212f20559d9
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2021-08-16 19:40:09 -0300

    Adding the spring project to the repository

---
+++ TimeControl/src/main/java/br/com/leomanzini/time/control/TimeControlApplication.java
@@ -0,0 +1,13 @@
+package br.com.leomanzini.time.control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeControlApplication.class, args);
+	}
+
+}
---
+++ TimeControl/src/test/java/br/com/leomanzini/time/control/TimeControlApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.leomanzini.time.control;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
