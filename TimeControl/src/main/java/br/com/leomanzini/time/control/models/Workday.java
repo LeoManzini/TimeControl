a6dc1fa2c94ad572788eed4f5191bedc094e435c
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2021-08-16 20:26:40 -0300

    Adding lombok to model package

--- TimeControl/src/main/java/br/com/leomanzini/time/control/models/Workday.java
+++ TimeControl/src/main/java/br/com/leomanzini/time/control/models/Workday.java
@@ -1,16 +1,20 @@
-package br.com.leomanzini.time.control.dtos;
+package br.com.leomanzini.time.control.models;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Builder
 public class Workday {
 	
 	private Long id;
