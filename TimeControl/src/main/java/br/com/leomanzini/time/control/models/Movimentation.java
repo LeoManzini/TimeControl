d928a0af93d171f303cf913eceb4283343434c7f
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2021-08-17 18:45:07 -0300

    Movimentation class made

--- TimeControl/src/main/java/br/com/leomanzini/time/control/models/Movimentation.java
+++ TimeControl/src/main/java/br/com/leomanzini/time/control/models/Movimentation.java
@@ -1,5 +1,12 @@
 package br.com.leomanzini.time.control.models;
 
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import javax.persistence.Embeddable;
+import javax.persistence.EmbeddedId;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,5 +18,23 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Movimentation {
 	
-	private String TODO;
+	@Data
+	@Embeddable
+	@NoArgsConstructor
+	@AllArgsConstructor
+	public class IdMovimentation implements Serializable {
+
+		private static final long serialVersionUID = 3465261450375508232L;
+		
+		private Long movimentationId;
+		private Long userId;
+	}
+	
+	@EmbeddedId
+	private IdMovimentation id;
+	private LocalDateTime startDate;
+	private LocalDateTime endDate;
+	private BigDecimal period;
+	private Occurrence occurrence;
+	private Calendar calendar;
 }
