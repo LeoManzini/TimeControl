b83fd724a74d7801cef6f61ff57a39ec22bbfe72
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2021-08-17 18:52:43 -0300

    AnnualLeave class created

---
+++ TimeControl/src/main/java/br/com/leomanzini/time/control/models/AnnualLeave.java
@@ -0,0 +1,39 @@
+package br.com.leomanzini.time.control.models;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import javax.persistence.Embeddable;
+import javax.persistence.EmbeddedId;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AnnualLeave {
+	
+	@Data
+	@Embeddable
+	@NoArgsConstructor
+	@AllArgsConstructor
+	public class IdAnnualLeave implements Serializable {
+
+		private static final long serialVersionUID = -8166512671194627096L;
+		
+		private Long annualLeaveId;
+		private Long movimentationId;
+		private Long userId;
+	}
+	
+	@EmbeddedId
+	private IdAnnualLeave id;
+	private LocalDateTime workedDate;
+	private BigDecimal hoursQuantity;
+	private BigDecimal hoursBalance;
+}
