83c2e70f345a9860ae4c86e7f04f246f22aa7eba
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2021-08-18 19:59:14 -0300

    Cleaning the project

--- TimeControl/src/main/java/br/com/leomanzini/time/control/controller/WorkdayController.java
+++ TimeControl/src/main/java/br/com/leomanzini/time/control/controller/WorkdayController.java
@@ -2,7 +2,6 @@ package br.com.leomanzini.time.control.controller;
 
 import java.util.List;
 
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
