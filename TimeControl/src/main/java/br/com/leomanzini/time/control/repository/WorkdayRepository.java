69584e16722c8392d5bd5c306bb290ba8f4b0899
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2021-08-17 22:09:15 -0300

    Stopped at class 10, crud made

--- TimeControl/src/main/java/br/com/leomanzini/time/control/repository/WorkdayRepository.java
+++ TimeControl/src/main/java/br/com/leomanzini/time/control/repository/WorkdayRepository.java
@@ -1,5 +1,11 @@
 package br.com.leomanzini.time.control.repository;
 
-public interface WorkdayRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.leomanzini.time.control.models.Workday;
+
+@Repository
+public interface WorkdayRepository extends JpaRepository<Workday, Long> {
 
 }
